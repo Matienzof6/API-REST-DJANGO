e55145aaf32b96b0da956a590618b248506eb935
Matias Pereiro <matienzof6@gmail.com>
2022-09-17 18:00:25 +0200

    Metodo POST y lectura del GET

--- Proyecto_API/API/views.py
+++ Proyecto_API/API/views.py
@@ -1,20 +1,43 @@
 from django.views import View
 from .models import Company
 from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 class CompanyView(View):
 
-    def get(self, request):
-        companies=list(Company.objects.values())
-        if len(companies)>0:
-            datos={'message':"Success",'companies':companies}
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if (id>0):
+            companies=list(Company.objects.filter(id=id).values())
+            if len(companies)>0:
+                company=companies[0]
+                datos={'message':"Success",'company':company}
+            else:
+                datos={'message':"Company not found"}
+            return JsonResponse(datos)
+
         else:
-            datos={'message':"Companies not found"}
-        return JsonResponse(datos)
+            companies=list(Company.objects.values())
+            if len(companies)>0:
+                datos={'message':"Success",'companies':companies}
+            else:
+                datos={'message':"Companies not found"}
+            return JsonResponse(datos)
 
     def post(self, request):
-        pass
+        # print(request.body)
+        jd=json.loads(request.body)
+        # print(jd)
+        Company.objects.create(name=jd['name'], website=jd['website'], foundation=jd['foundation'])
+        datos={'message':"Success"}
+        return JsonResponse(datos)
+
 
     def put(self, request):
         pass
