8325ad2af702ad5d741c0d33dfb9af2636ed8a7c
Matias Pereiro <matienzof6@gmail.com>
2023-07-21 01:28:04 +0200

    :wq

--- Proyecto_API/API/urls.py
+++ Proyecto_API/API/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CompanyView
 urlpatterns = [
     path('companies/',CompanyView.as_view(), name='companies_list'),
-    path('companies/<int:id>',CompanyView.as_view(), name='companies_process'),
+    path('companies/<int:id>',CompanyView.as_view(), name='companies_process'), #Esto hace que podamos listar de una a una las compañias mediante el id
 ]
 
